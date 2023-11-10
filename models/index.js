97609ea9ed3d6776674b81126c7c7b6fc981c40f
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-10 22:01:48 +0800

    Add files for posts model

---
+++ models/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const Posts = require("./posts");
+async function init() {
+  await Posts.sync();
+}
+
+init();
+
+module.exports = {
+  Posts,
+};
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+require('dotenv').config();
+let dbConnect = require("./dbConnect");
+
+
 const port = process.env.PORT || 8080;
 
 app.listen(port,()=>{
